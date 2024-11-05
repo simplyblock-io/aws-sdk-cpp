b8abefdefe5d2114e6acc5e0748693712b3dad54
ashwin-simplyblock <ash@simplyblock.io>
2024-11-04 20:48:25 -0600

    add full install to s3 build script

--- build_s3_sdk.sh
+++ build_s3_sdk.sh
@@ -1,6 +1,6 @@
 cd ~/aws-sdk-cpp && mkdir -p sdk_build
 
-yes | sudo hwclock --hctosys && yes | sudo apt update && yes | sudo apt upgrade && yes | sudo apt install curl && yes | sudo apt-get install libcurl4-openssl-dev
+yes | sudo hwclock --hctosys && yes | sudo apt update && yes | sudo apt upgrade && yes | sudo apt install curl && yes | sudo apt-get install libcurl-devel openssl-devel libuuid-devel pulseaudio-libs-devel
 
 cd ~/aws-sdk-cpp/sdk_build && cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DFORCE_SHARED_CRT=OFF -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DENABLE_TESTING=OFF -DBUILD_ONLY="s3"
 
