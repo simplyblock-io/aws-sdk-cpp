e1253e3b4e1ed68af829ab90d01b679ca3eda4ad
ashwin-simplyblock <ash@simplyblock.io>
2024-12-01 02:08:54 -0600

    use better-performant, non-legacy crt http client

--- build_s3_sdk_deb_or_ubuntu.sh
+++ build_s3_sdk_deb_or_ubuntu.sh
@@ -2,7 +2,7 @@ cd ~/aws-sdk-cpp && mkdir -p sdk_build
 
 yes | sudo hwclock --hctosys && yes | sudo apt update && yes | sudo apt upgrade && yes | sudo apt install curl && yes | sudo apt-get install libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev
 
-cd ~/aws-sdk-cpp/sdk_build && cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DFORCE_SHARED_CRT=OFF -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DENABLE_TESTING=OFF -DBUILD_ONLY="s3"
+cd ~/aws-sdk-cpp/sdk_build && cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DFORCE_SHARED_CRT=OFF -DUSE_CRT_HTTP_CLIENT=ON -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DENABLE_TESTING=OFF -DBUILD_ONLY="s3"
 
 cd ~/aws-sdk-cpp/sdk_build && make
 
--- build_s3_sdk_not_deb_or_ubuntu.sh
+++ build_s3_sdk_not_deb_or_ubuntu.sh
@@ -2,7 +2,7 @@ cd ~/aws-sdk-cpp && mkdir -p sdk_build
 
 yes | sudo yum update && yes | sudo yum upgrade && yes | sudo yum install curl && yes | sudo yum install libcurl-devel openssl-devel libuuid-devel pulseaudio-libs-devel
 
-cd ~/aws-sdk-cpp/sdk_build && cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DFORCE_SHARED_CRT=OFF -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DENABLE_TESTING=OFF -DBUILD_ONLY="s3"
+cd ~/aws-sdk-cpp/sdk_build && cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DFORCE_SHARED_CRT=OFF -DUSE_CRT_HTTP_CLIENT=ON -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DENABLE_TESTING=OFF -DBUILD_ONLY="s3"
 
 cd ~/aws-sdk-cpp/sdk_build && make
 
